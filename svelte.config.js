ba82073a2309b545e3dd36cfa2f2445069ae2432
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-04 20:39:42 +0100

    feat: switched to adapter-static for prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter()
     }
 };
-
-export default config;
