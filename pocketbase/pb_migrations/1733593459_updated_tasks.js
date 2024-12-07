85413b396572c2433fa788be3be9b9cf336d54f3
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-07 18:49:40 +0100

    feat(tasks): added isCompleted property

---
+++ pocketbase/pb_migrations/1733593459_updated_tasks.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "bool56683945",
+    "name": "isCompleted",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2602490748")
+
+  // remove field
+  collection.fields.removeById("bool56683945")
+
+  return app.save(collection)
+})
